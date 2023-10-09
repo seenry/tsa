0c9fa5979744b2de391524086d140de1059be20f
Ubuntu <ubuntu@ip-172-31-38-77.us-east-2.compute.internal>
2023-10-09 14:28:34 +0000

    hack

--- include/gpu_network.h
+++ include/gpu_network.h
@@ -1,20 +1,25 @@
 #pragma once
 
-#include <vector>
-
 #include "nccl.h"
 
 class GPUNetwork
 {
 public:
-    void AllGather();
     void Initialize(int buffer_size);
+    
+    void Point2Point(); // Measure all p2p connections
+    void AllGather(); //
 private:
-    int n_dev;
+    void Point2PointSingle(int from, int to);
+    void Point2PointBidirectional(int rank_1, int rank_2);
 
-    ncclComm_t comms;
-    cudaStream_t* streams;
-    char** buffers;
-    char** host_buffers;
-    cudaEvent_t timers;
+    int n_gpus;
+    int my_rank;
+
+    ncclComm_t comm;
+    cudaStream_t stream;
+    char* buffer;
+    char* host_buffer;
+    cudaEvent_t start_timer;
+    cudaEvent_t stop_timer;
 };
--- src/gpu_network.cpp
+++ src/gpu_network.cpp
@@ -6,23 +6,23 @@
 #include "gpu_network.h"
 
 void GPUNetwork::Initialize(int buffer_size) {
-    CUDACHECK(cudaGetDeviceCount(&n_dev));
-    int* dev_ids = new int[n_dev];
-    for (int i = 0; i < n_dev; i++) {
+    CUDACHECK(cudaGetDeviceCount(&n_gpus));
+    int* dev_ids = new int[n_gpus];
+    for (int i = 0; i < n_gpus; i++) {
         dev_ids[i] = i;
     }
 
-    streams = new cudaStream_t[n_dev];
-    buffers = new std::vector<char>[n_dev];
-    host_buffers = new std::vector<char>
-    for (int i = 0; i < n_dev; i++) {
-        CUDACHECK(cudaSetDevice(i));
-        CUDACHECK(cudaMalloc(buffers + i, buffer_size * sizeof(char)));
-        CUDACHECK(cudaMemset(buffers[i], i, buffer_size * sizeof(char)));
-        CUDACHECK(cudaStreamCreate(streams + i));
-    }
+    streams = ;
+    buffers = ;
+    host_buffers = ;
+    
 
-    NCCLCHECK(ncclCommInitAll(&comms, n_dev, dev_ids));
+    CUDACHECK(cudaSetDevice(MY_RANK));
+    CUDACHECK(cudaMalloc(&buffer, buffer_size * sizeof(char)));
+    CUDACHECK(cudaMemset(&buffer, i, buffer_size * sizeof(char)));
+    CUDACHECK(cudaStreamCreate(streams + i));
+
+    NCCLCHECK(ncclCommInitAll(&comms, n_gpus, dev_ids));
     delete[] dev_ids;
 }
 
